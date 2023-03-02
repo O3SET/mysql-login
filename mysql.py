0b13c3eec95782643696e918ad93d359a6143000
O3SET <77259018+O3SET@users.noreply.github.com>
2023-03-02 09:41:38 +0800

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,17 +1,10 @@
 import mysql.connector
 import os
-import signal
 
 # MySQL连接信息
 host = 'localhost'
 user = 'root'
 password = 'password'
-
-# 信号处理函数
-def handler(signum, frame):
-    print("查询超时，终止查询操作")
-    cursor.cancel()
-    result = None
 
 # 遍历文件
 filename = '3306.txt'
@@ -23,20 +16,6 @@ with open(filename, 'r') as f:
             # 尝试连接MySQL数据库
             cnx = mysql.connector.connect(host=ip, user=username, password=pwd)
             print(f"成功连接到MySQL数据库，IP: {ip}, 用户名: {username}, 密码: {pwd}")
-
-            # 执行status命令
-            cursor = cnx.cursor()
-            signal.signal(signal.SIGALRM, handler)
-            signal.alarm(10)  # 设置超时时间为10秒
-            cursor.execute("status")
-            signal.alarm(0)  # 取消超时时间
-            result = cursor.fetchone()
-
-            if result is not None:
-                print(f"status命令执行结果：{result}")
-            else:
-                print("查询超时，继续下一个查询")
-
             cnx.close()
         except mysql.connector.Error as err:
             # 连接失败，打印错误信息
